cf2ad196206bc80f93d0213e45e08b7acd7f9c6b
danielfatec2006 <daniel.silva397@fatec.sp.gov.br>
2024-06-24 05:28:28 -0300

    Implementar Auditoria de Mudanças no Estoque

---
+++ StockApp.Application/Services/AuditService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using StockApp.Application.Interfaces;
+using StockApp.Domain.Interfaces;
+using System.Threading.Tasks;
+
+namespace StockApp.Application.Services
+{
+    public class AuditService : IAuditService
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly ILogger<AuditService> _logger;
+
+        public AuditService(IProductRepository productRepository, ILogger<AuditService> logger)
+        {
+            _productRepository = productRepository;
+            _logger = logger;
+        }
+
+        public async Task AuditStockChange(int productId, int oldStock, int newStock)
+        {
+            var product = await _productRepository.GetByIdAsync(productId);
+
+            _logger.LogInformation($"Audit stock change - Product: {product.Name}, Old Stock: {oldStock}, New Stock: {newStock}");
+        }
+    }
+}
