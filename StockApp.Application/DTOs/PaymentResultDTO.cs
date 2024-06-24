c405ad8d3711cb7c7763df3f5673f6bc17d92303
vicdsso <vicdsso@gmail.com>
2024-06-23 22:40:04 -0300

    Implementar Integração com Serviço de Pagamentos Externo

---
+++ StockApp.Application/DTOs/PaymentResultDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp.Application.DTOs
+{
+    public class PaymentResultDTO
+    {
+        public string TransactionId { get; set; }
+        public string Status { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ StockApp.Application/Services/PaymentIntegrationService.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using StockApp.Application.DTOs;
+using StockApp.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp.Application.Services
+{
+    public class PaymentIntegrationService : IPaymentIntegrationService
+    {
+        private readonly HttpClient _httpClient;
+        public PaymentIntegrationService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<PaymentResultDTO> ProcessPaymentAsync(PaymentRequestDTO paymentRequest)
+        {
+            var response = await _httpClient.PostAsJsonAsync("process", paymentRequest);
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            var paymentResult = JsonConvert.DeserializeObject<PaymentResultDTO>(content);
+
+            return paymentResult;
+        }
+    }
+}
