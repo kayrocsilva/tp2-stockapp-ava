c764392736c178d725756b1d440e87a21ce001b0
kamilyfatec <kamily.simeao@fatec.sp.gov.br>
2024-06-24 07:11:12 -0300

    Implementar Integração com Serviço de Análise de Dados

---
+++ StockApp.Application/DTOs/DataAnalysisDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp.Application.DTOs
+{
+    public class DataAnalysisDTO
+    {
+        public string AnalysisId { get; set; }
+        public DateTime AnalysisDate { get; set; }
+        public string Result { get; set; }
+        // Outros campos relevantes para o resultado da análise
+
+        // Construtor vazio necessário para desserialização JSON
+        public DataAnalysisDTO()
+        {
+        }
+    }
+}
---
+++ StockApp.Application/Services/DataAnalysisService.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using StockApp.Application.DTOs;
+using StockApp.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp.Application.Services
+{
+    public class DataAnalysisService : IDataAnalysisService
+    {
+        private readonly HttpClient _httpClient;
+
+        public DataAnalysisService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<DataAnalysisDTO> AnalyzeDataAsync()
+        {
+            var response = await _httpClient.GetAsync("analyze");
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            var analysis = JsonConvert.DeserializeObject<DataAnalysisDTO>(content);
+
+            return analysis;
+        }
+    }
+}
