12aee2b76f1a61695ba805ca939e9847d4ed19f6
kamilyfatec <kamily.simeao@fatec.sp.gov.br>
2024-06-24 07:26:36 -0300

    Implementar Sistema de Análise de Desempenho de Vendas

---
+++ StockApp.Application/DTOs/SalesPerformanceDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp.Application.DTOs
+{
+    public class SalesPerformanceDTO
+    {
+        public decimal TotalSales { get; set; }
+        public int TotalOrders { get; set; }
+        public decimal AverageOrderValue { get; set; }
+        // Outros campos relevantes para o desempenho de vendas
+
+        // Construtor vazio necessário para desserialização JSON
+        public SalesPerformanceDTO()
+        {
+        }
+    }
+}
