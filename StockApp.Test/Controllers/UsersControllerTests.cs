7c7837105b6a350c3dd309d52a100c8cba692a06
kayrocsilva <kayro.silva@fatec.sp.gov.br>
2024-06-24 03:24:18 -0300

    Adicionar Testes Unitários para o Controlador UsersController

---
+++ StockApp.Test/Controllers/UsersControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using StockApp.API.Controllers;
+using StockApp.Application.DTOs;
+using StockApp.Domain.Entities;
+using StockApp.Domain.Interfaces;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace StockApp.UnitTests.Controllers
+{
+    public class UsersControllerTests
+    {
+        [Fact]
+        public async Task Register_ValidUser_ReturnsOk()
+        {
+            // Arrange
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var usersController = new UsersController(userRepositoryMock.Object);
+
+            var userRegisterDto = new UserRegisterDTO
+            {
+                UserName = "testuser",
+                Password = "password",
+                Role = "User"
+            };
+
+            // Act
+            var result = await usersController.Register(userRegisterDto) as OkResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(200, result.StatusCode);
+
+            // Verify repository method was called
+            userRepositoryMock.Verify(repo => repo.AddAsync(It.IsAny<User>()), Times.Once);
+        }
+    }
+}
